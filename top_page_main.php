3c2ebb174e6c2da3ec97b224bccd1a8d4eb22820
BuildTools <unconfigured@null.spigotmc.org>
2024-04-27 09:44:40 +0800

    Sign up page

---
+++ top_page_main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main Page</title>
+    <link rel="stylesheet" href="style/top_page.css">
+</head>
+<body>
+    <header class="main">
+        <section class="top_page">
+            <a href="#">  <img class="logo" src="resources/LOGO_FN3.png"></a>
+                <nav>
+                    <a href="#" class="active">Home</a>
+                    <a href="#about_us" >About us</a>
+                    <a href="#" >Tutors</a>
+                    <a href="#">Our Team</a>
+                </nav>
+        </section>
+        <section class="btn">
+            <button class="login-btn">
+                Login
+            </button>
+            <button class="rgstr-btn">
+                Register
+            </button>
+        </section>
+    </header>
+</body>
+</html>
